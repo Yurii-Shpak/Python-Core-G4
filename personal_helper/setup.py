7cee5ce7621e740a8212ef9e7fef9c3f0b083188
Yurii-Shpak <yshpak.gora@gmail.com>
2022-04-22 13:06:24 +0300

    Personal Helper

---
+++ personal_helper/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='personal_helper',
+    version='0.0.1',
+    description='Personal helper bot that works in 3 modes: contacts book, notes and files sorting in a selected folder.',
+    url='https://github.com/Yurii-Shpak/Python-Core-G4/tree/main/personal_helper',
+    author='Yurii Shpak',
+    author_email='yshpak.gora@gmail.com',
+    license='MIT',
+    packages=find_packages(),
+    entry_points={'console_scripts': ['personal-helper = personal_helper.personal_helper:main']}
+)
