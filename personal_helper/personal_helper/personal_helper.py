7d410d171c1045cf3f5c2224d60350f81e1084c3
Yurii-Shpak <yshpak.gora@gmail.com>
2022-04-22 19:34:44 +0300

    Personal Helper

--- personal_helper/personal_helper/personal_helper.py
+++ personal_helper/personal_helper/personal_helper.py
@@ -1,6 +1,112 @@
+from collections import UserDict
+import os.path
+import pickle
+
+
+class AddressBook(UserDict):
+
+    def load_from_file(self, file_name):
+        if os.path.exists(file_name):
+            with open(file_name, 'rb') as fh:
+                self.data = pickle.load(fh)
+                if len(self.data):
+                    return f'The contacts book is loaded from the file "{file_name}".'
+                else:
+                    return 'This is empty contacts book. Add contacts to it.'
+        else:
+            return 'This is empty contacts book. Add contacts into it.'
+
+    def save_to_file(self, file_name):
+        with open(file_name, 'wb') as fh:
+            pickle.dump(self.data, fh)
+        return f'The contacts book is saved in the file "{file_name}".'
+
+
+contacts = AddressBook()
+
+
+class Record:
+
+    def __init__(self, name, address=None, phones_list=[], email=None, birthday=None):
+        self.name = name
+        self.address = address
+        self.phones_list = phones_list
+        self.email = email
+        self.birthday = birthday
+
+
+def input_error(func):
+
+    def inner(command_line):
+
+        try:
+            result = func(command_line)
+        except:
+            if func.__name__ == 'save_func':
+                result = f'Error while saving.'
+        return result
+
+    return inner
+
+
+@input_error
+def exit_func(command_line):
+
+    return 'Good bye!'
+
+
+@input_error
+def save_func(command_line):
+
+    return contacts.save_to_file('contacts.bin')
+
+
+COMMANDS = {
+    'close': exit_func,
+    'exit': exit_func,
+    'good bye': exit_func,
+    'save': save_func,
+}
+
+
+ONE_WORD_COMMANDS = ['close', 'exit', 'save']
+TWO_WORDS_COMMANDS = ['good bye']
+
+
+def get_handler(command):
+    return COMMANDS[command]
+
+
 def main():
 
-    pass
+    print(contacts.load_from_file('contacts.bin'))
+
+    while True:
+        command_line = []
+        while not command_line:
+            command_line = input('>>> ').split()
+
+        right_command = False
+
+        if len(command_line) > 1 and \
+           f'{command_line[0].lower()} {command_line[1].lower()}' in TWO_WORDS_COMMANDS:
+            command = f'{command_line.pop(0).lower()} {command_line.pop(0).lower()}'
+            right_command = True
+
+        if not right_command:
+            command = command_line.pop(0).lower()
+            right_command = command in ONE_WORD_COMMANDS
+
+        if not right_command:
+            print(
+                f'The "{command}" command is wrong! The allowable commands are {", ".join(ONE_WORD_COMMANDS + TWO_WORDS_COMMANDS)}.')
+            continue
+
+        handler = get_handler(command)
+        print(handler(command_line))
+        if handler is exit_func:
+            print(contacts.save_to_file('contacts.bin'))
+            break
 
 
 if __name__ == '__main__':
